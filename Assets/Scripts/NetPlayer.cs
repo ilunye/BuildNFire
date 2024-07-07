fd64e377136dd4a7cbfaed21cd930586f78ed408
ilunye <ilunye93@qq.com>
2024-07-07 18:18:53 +0800

    network prototype

--- Assets/Scripts/NetPlayer.cs
+++ Assets/Scripts/NetPlayer.cs
@@ -5,9 +5,21 @@ using Mirror;
 
 public class NetPlayer : NetworkBehaviour
 {
+    private GameObject myCannon;
+    [Command]
+    private void CmdNextState(){
+        myCannon.GetComponent<Cannon>().next_state();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        for(int i=0; i<4; i++){
+            myCannon = GameObject.Find("cannon" + i.ToString());
+            if(myCannon.GetComponent<Cannon>().claimed == false){
+                myCannon.GetComponent<Cannon>().claimed = true;
+                break;
+            }
+        } 
         
     }
 
@@ -16,5 +28,8 @@ public class NetPlayer : NetworkBehaviour
     {
         if(!isLocalPlayer) return;
         transform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, 0, Input.GetAxis("Vertical") * Time.deltaTime));
+        if(Input.GetKeyDown(KeyCode.E)){
+            CmdNextState();
+        }
     }
 }
