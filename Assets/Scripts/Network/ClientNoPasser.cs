f5b2369dd38ff8ba5e3c8baebb47915de2cd6675
ilunye <ilunye93@qq.com>
2024-07-09 15:17:17 +0800

    LRM support for webgl host issue

---
+++ Assets/Scripts/Network/ClientNoPasser.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LightReflectiveMirror;
+using Mirror;
+
+public class ClientNoPasser : MonoBehaviour
+{
+    private LightReflectiveMirrorTransport lrm;
+    private NetworkManagerHUD nmh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lrm = GetComponent<LightReflectiveMirrorTransport>();
+        nmh = GetComponent<NetworkManagerHUD>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nmh.clientNo = lrm.serverId; 
+    }
+}
