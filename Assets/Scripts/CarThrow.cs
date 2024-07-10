40a5642d74874da35b1ab61ff5f8eabe4d876492
shanying <sy13069648212@163.com>
2024-07-10 21:03:23 +0800

    feat(buff)拉取buff prefab

--- Assets/Scripts/CarThrow.cs
+++ Assets/Scripts/CarThrow.cs
@@ -27,15 +27,18 @@ public class test : MonoBehaviour
     if (timer >= interval)
     {
         GameObject c;
-        int r=Random.Range(0,3);
+        int r=Random.Range(0,4);//决定抛出物体
         if(r==0){
             c = Instantiate(Resources.Load("prefabs/Bomb Red") as GameObject);
         }
         else if(r==1){
-            c = Instantiate(Resources.Load("prefabs/Bomb Red") as GameObject);
+            c = Instantiate(Resources.Load("prefabs/burger_1_lod0") as GameObject);
+        }
+        else if(r==2){
+            c = Instantiate(Resources.Load("prefabs/Hourglass Green") as GameObject);
         }
         else{
-            c = Instantiate(Resources.Load("prefabs/Bomb Red") as GameObject);
+            c = Instantiate(Resources.Load("prefabs/Lock Silver") as GameObject);
         }
         
         Debug.Log("扔物体");
@@ -43,7 +46,7 @@ public class test : MonoBehaviour
         Rigidbody cubeRigidbody = c.GetComponent<Rigidbody>();
         cubeRigidbody.AddForce(Vector3.up * throwForce, ForceMode.Impulse);
 
-        StartCoroutine(RemoveRigidbodyAfterDelay(cubeRigidbody, 1.5f)); // 延时两秒后移除 Rigidbody
+        StartCoroutine(RemoveRigidbodyAfterDelay(cubeRigidbody, 1.5f)); // 延时1.5秒后移除 Rigidbody
 
         timer = 0f;
         interval = 2.5f * Random.Range(1, 3);
