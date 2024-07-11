d98a41f7818580eb254f04fccd338740adae9f92
shanying <sy13069648212@163.com>
2024-07-11 14:17:42 +0800

    feat(workflow):添加ui界面

---
+++ Assets/Scripts/WorkFlow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class WorkFlow : MonoBehaviour
+{
+    public int wood_number;
+    public int iron_number;
+    public int gunpowder_number;
+    public int projectile_number;
+    public TMP_Text wood_text;
+    public TMP_Text iron_text;
+    public TMP_Text gunpowder_text;
+    public TMP_Text projectile_text;
+    public GameObject frame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
