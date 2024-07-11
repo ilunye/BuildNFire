c14498b8841be2c7e54ec4505c41c738f2121924
ilunye <ilunye93@qq.com>
2024-07-12 02:53:20 +0800

    change traffic density

--- Assets/Scripts/TrafficControl.cs
+++ Assets/Scripts/TrafficControl.cs
@@ -7,18 +7,13 @@ public class TrafficControl : MonoBehaviour
     private float interval_green;
     private float interval_red;
     private Vector3 spawnPosition = new Vector3(778.22f, 0.0f, 966.79f);
-    private int p1,p2;
     public float timer_green;
     public float timer_red;
 
     void Start()
     {
-        interval_green=2f;
-        interval_red=3f;
-        p1=(int)Random.Range(1,2);
-        p2=(int)Random.Range(1,2);
-        interval_green=interval_green*p1;
-        interval_red=interval_red*p2;
+        interval_green=Random.Range(4,10);
+        interval_red=Random.Range(4,10);
     }
 
      void Update()
