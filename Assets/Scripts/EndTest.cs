abda5a6de9e9b72d86afe59b7dffe536f257034c
shanying <sy13069648212@163.com>
2024-07-15 00:58:41 +0800

    feat(explosion)

--- Assets/Scripts/EndTest.cs
+++ Assets/Scripts/EndTest.cs
@@ -5,32 +5,33 @@ using UnityEngine;
 public class EndTest : MonoBehaviour
 {
     private float explosionForce=10f;
-    public float explosionRadius = 4f;
+    public float explosionRadius = 10f;
+    public int status;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        status=0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.B)){//左侧失败
-            Debug.Log("左侧");
-            GameObject boom = Instantiate(Resources.Load("prefabs/Explosion single fallback") as GameObject);
+        if(status==1){//左侧失败
+            /*Debug.Log("左侧");
+            GameObject boom = Instantiate(Resources.Load("prefabs/explosion") as GameObject);
             boom.transform.position=new Vector3(794.84f,0f,980.3f);
-            GameObject fire = Instantiate(Resources.Load("prefabs/Fire fuel sharp") as GameObject);
-            fire.transform.position=new Vector3(794.84f,0f,980.3f);
+            GameObject fire = Instantiate(Resources.Load("prefabs/fire") as GameObject);
+            fire.transform.position=new Vector3(794.84f,0f,980.3f);*/
             ExplosionLeft(explosionForce,new Vector3(794.84f,0f,980.3f),explosionRadius);
 
         }
-        if(Input.GetKey(KeyCode.N)){//右侧失败
-            Debug.Log("右侧");
-            GameObject boom = Instantiate(Resources.Load("prefabs/Explosion single fallback") as GameObject);
+        if(status==2){//右侧失败
+            /*Debug.Log("右侧");
+            GameObject boom = Instantiate(Resources.Load("prefabs/explosion") as GameObject);
             boom.transform.position=new Vector3(804.42f,0f,980.3f);
-            GameObject fire = Instantiate(Resources.Load("prefabs/Fire fuel sharp") as GameObject);
-            fire.transform.position=new Vector3(804.42f,0f,980.3f);
+            GameObject fire = Instantiate(Resources.Load("prefabs/fire") as GameObject);
+            fire.transform.position=new Vector3(804.42f,0f,980.3f);*/
             ExplosionRight(explosionForce,new Vector3(804.42f,0f,980.3f),explosionRadius);
         }
         
