7b6e100cc877d46f2a48e7971b1507a2e5b74996
shanying <sy13069648212@163.com>
2024-07-16 00:17:05 +0800

    feat(instruction)change UI instruction

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,23 +2,56 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public TextMeshProUGUI text;
+    public Image[] images;   // 存储Image组件的数组
+    private int currentIndex; 
     private Color initialColor;
-
+    private bool confirmButtonEnabled = false;  //15秒后才能按confirm
+    private float startTime;
     void Start()
     {
         initialColor = text.color;
+        currentIndex = 0;
+        startTime = Time.time; 
+        ShowCurrentImage();
     }
 
     void Update()
     {
+        if (!confirmButtonEnabled && Time.time - startTime >= 15f)
+        {
+            confirmButtonEnabled = true;
+        }
         if (Input.GetKeyUp(KeyCode.Escape))
         {
             Application.Quit();
         }
+    }
+    public void NextImage()
+    {
+        currentIndex = (currentIndex + 1) % 3;
+        ShowCurrentImage();
+    }
+    private void ShowCurrentImage()
+    {
+        // 隐藏所有图片
+        foreach (Image image in images)
+        {
+            image.gameObject.SetActive(false);
+        }
+
+        // 显示当前索引的图片
+        images[currentIndex].gameObject.SetActive(true);
+    }
+
+    public void LastImage()
+    {
+        currentIndex = (currentIndex - 1 + images.Length) % images.Length;
+        ShowCurrentImage();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -37,8 +70,9 @@ public class Menu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IP
         {
             SceneManager.LoadScene("Scenes/pre");
         }
-        else if (text.text == "Confirm")
+        else if (text.text == "confirm"&&confirmButtonEnabled) //15秒后可点击
         {
+            //Debug.Log("confirm");
             SceneManager.LoadScene("Scenes/Main");
         }
         else if (text.text == "instruction")
@@ -52,6 +86,16 @@ public class Menu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IP
         else if (text.text == "EXIT")
         {
             Application.Quit();
+        }
+        else if (text.text == "next")
+        {
+            //Debug.Log("next");
+            NextImage();
+        }
+        else if (text.text == "last")
+        {
+            //Debug.Log("last");
+            LastImage();
         }
     }
 }
