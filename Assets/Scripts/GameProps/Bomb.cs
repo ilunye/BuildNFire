c53a7ba9993600130af5ef05741d465edffeabc1
whitezhiiii <zero2645907003@163.com>
2024-07-17 19:52:22 +0800

    fix(bug):fix fire cannon bug and reset the explosion range of bomb

--- Assets/Scripts/GameProps/Bomb.cs
+++ Assets/Scripts/GameProps/Bomb.cs
@@ -6,7 +6,7 @@ public class Bomb : MonoBehaviour
 {
     private float delay = 10f; // 炸弹的延迟时间
     private float explosionForce = 400f; // 爆炸的力量
-    private float explosionRadius = 1f; // 爆炸的半径
+    private float explosionRadius = 1.5f; // 爆炸的半径
     public GameObject explosionEffect; // 爆炸效果的预制体
 
     public float countdown; //爆炸倒计时
