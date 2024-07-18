9af6e39b8ff7f2c868e35a15980ad014c638fdae
ilunye <ilunye93@qq.com>
2024-07-19 01:45:35 +0800

    bomb host done, client still bug; car hit

--- Assets/Scripts/Network/GameProps/NetThrowBomb.cs
+++ Assets/Scripts/Network/GameProps/NetThrowBomb.cs
@@ -1,4 +1,3 @@
-using UnityEditor;
 using UnityEngine;
 using System.Collections;
 using Mirror;
@@ -22,9 +21,42 @@ public class NetThrowBomb : NetworkBehaviour
     // public GameObject BombImage;     //存放炸弹的显示图像
 
     private bool readytothrow = false;
-    private GameObject theBombTarget = null;
+    public GameObject theBombTarget = null;
 
     private KeyCode keyCodeE;
+
+    public float changeScale = 1f;
+
+    private float startHigh = 0.0001f;
+    public int sceneID = 1;
+    private float multipleFore = 3f;
+    private Vector3 upVector = new Vector3(0f, 0f, 0f);
+
+    public bool target_dist_increasing = true;
+
+    [Command(requiresAuthority = false)]
+    public void CmdServerSpawn(string path, Vector3 position, bool isTarget, bool isBomb)
+    {
+        if(isTarget){
+            theBombTarget = Instantiate(Resources.Load(path) as GameObject, position, Quaternion.Euler(90, 0, 0));
+            NetworkServer.Spawn(theBombTarget);
+        }else if(isBomb){
+            bomb = Instantiate(Resources.Load(path) as GameObject, position, Quaternion.identity);
+            bomb.GetComponent<NetBomb>().SetTarget(theBombTarget);        // set the bomb target for destroy
+            NetworkServer.Spawn(bomb);
+            StartCoroutine(ThrowBombPosition(position, transform.forward));
+        }
+    }
+    [ClientRpc]
+    public void RpcSetTarget(GameObject target)
+    {
+        theBombTarget = target;
+    }
+    [Command(requiresAuthority = false)]
+    public void CmdDestroy(GameObject obj)
+    {
+        NetworkServer.Destroy(obj);
+    }
 
     void Start()
     {
@@ -38,25 +70,52 @@ public class NetThrowBomb : NetworkBehaviour
         {
             keyCodeE = KeyCode.Return;
         }
-       
+        switch (sceneID)
+        {
+            case 2:
+                startHigh = 1.2f;
+                MaxThrowForce = 20f;
+                multipleFore = 7f;
+                upVector = new Vector3(0f, 0.3f, 0f); //make sure the bomb target is above the ground
+                break;
+        }
+
+
     }
     void Update()
     {
+        if(!isLocalPlayer) return;
         if (GetComponent<NetCharacter>().Material == NetCharacter.MaterialType.Bomb) //如果玩家捡到炸弹
         {
             // BombImage.SetActive(true);
             if (Input.GetKey(keyCodeE)) //如果长按E则累计投掷的力量
             {
-                if(!readytothrow){      // instantiate a target point
+                if (!readytothrow)
+                {      // instantiate a target point
+                    //Debug.Log("creat bomb target" );
                     readytothrow = true;
-                    GameObject target = Instantiate(Resources.Load("Prefabs/bomb_target") as GameObject);
-                    target.transform.position = transform.position + transform.forward * 1f * throwForce;
-                    theBombTarget = target;
-                }else
-                    theBombTarget.transform.position = transform.position + transform.forward * (1f * throwForce + 0.6f);
-                if (throwForce < MaxThrowForce)
+
+                    //Debug.Log(target.transform.position);
+                    CmdServerSpawn("Prefabs/Online/bomb_target", transform.position + transform.forward * 1f * throwForce + upVector, true, false);
+                }
+                else if(theBombTarget){
+                    theBombTarget.transform.position = transform.position + transform.forward * (1f * throwForce + 0.6f) + upVector;
+                }
+                if (target_dist_increasing && throwForce < MaxThrowForce)
                 {
-                    throwForce += Time.deltaTime * 3f;
+                    throwForce += Time.deltaTime * multipleFore;
+                    if(throwForce > MaxThrowForce)
+                        throwForce = MaxThrowForce;
+                    if(throwForce == MaxThrowForce)
+                        target_dist_increasing = false;
+                }
+                else if(!target_dist_increasing && throwForce > InitthrowForce)
+                {
+                    throwForce -= Time.deltaTime * multipleFore;
+                    if(throwForce < InitthrowForce)
+                        throwForce = InitthrowForce;
+                    if(throwForce == InitthrowForce)
+                        target_dist_increasing = true;
                 }
             }
 
@@ -64,12 +123,12 @@ public class NetThrowBomb : NetworkBehaviour
             {
                 readytothrow = false;
                 Throw();
-                GetComponent<NetCharacter>().Material =NetCharacter.MaterialType.None;
+                GetComponent<NetCharacter>().Material = NetCharacter.MaterialType.None;
 
             }
         }
         // else
-            // BombImage.SetActive(false);
+        // BombImage.SetActive(false);
 
 
     }
@@ -79,7 +138,7 @@ public class NetThrowBomb : NetworkBehaviour
     {
         Vector3 p = new Vector3(0f, 2f, 1f);
         GameObject bomb = Instantiate(Resources.Load("Prefabs/Bomb Red") as GameObject); // 创建炸弹实例
-        bomb.transform.localPosition = gameObject.transform.localPosition + p;
+        bomb.transform.position = gameObject.transform.position + p;
         Rigidbody rb = bomb.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange); // 给炸弹一个向前的力,但不会改变炸弹的位置
     }
@@ -91,21 +150,15 @@ public class NetThrowBomb : NetworkBehaviour
     void Throw()
     {
         hasthrow = true;
-        bomb = Instantiate(Resources.Load("Prefabs/Bomb Red") as GameObject); // 创建炸弹实例
-        // 设置炸弹的初始位置
-        bomb.transform.localPosition = gameObject.transform.localPosition + gameObject.transform.forward * 0.5f;
-        Vector3 startPosition = bomb.transform.localPosition;
-
-        bomb.GetComponent<Bomb>().SetTarget(theBombTarget);        // set the bomb target for destroy
-
+        Vector3 startPosition = gameObject.transform.position + gameObject.transform.forward * 0.5f + new Vector3(0, 0.5f, 0);
+        CmdServerSpawn("Prefabs/Online/Bomb Red", startPosition, false, true);
         // 投掷炸弹，以一定的速度沿着投掷方向移动
-        StartCoroutine(ThrowBombPosition(startPosition, transform.forward));
 
     }
 
     IEnumerator ThrowBombPosition(Vector3 startPosition, Vector3 direction)
     {
-        //Debug.Log("開始位置" + startPosition);
+        //Debug.Log("direction:" + direction);
         elapsedTime = 0f;
 
         Vector3 gravity; //增加重力
@@ -114,17 +167,17 @@ public class NetThrowBomb : NetworkBehaviour
         //Debug.Log("throwforce1 = " + throwForce);
         while (elapsedTime < throwDuration && bomb)
         {
-            //Debug.Log("循环" + elapsedTime);
+
             // 根据投掷速度和时间计算新的位置
             float x = throwForce * elapsedTime * direction.x;
             float z = throwForce * elapsedTime * direction.z;
-            float y = 0.0001f + 5f * elapsedTime - 10.0f * 0.5f * Mathf.Pow(elapsedTime, 2);
+            float y = startHigh + 5f * elapsedTime - 10f * 0.5f * Mathf.Pow(elapsedTime, 2);
             gravity = new Vector3(x, y, z);
+            //Debug.Log("y" + y);
             Vector3 newPosition = startPosition + gravity;
-
             // 更新炸弹的位置
             bomb.transform.position = newPosition;
-            //Debug.Log("炸弹的当前位置为：" + bomb.transform.localPosition);
+            //Debug.Log("炸弹的当前位置为：" + bomb.transform.position);
 
             elapsedTime += Time.deltaTime;
             yield return null;
@@ -136,8 +189,9 @@ public class NetThrowBomb : NetworkBehaviour
 
     public void SetTarget(GameObject target)
     {
-        if(target == null){
-            Destroy(theBombTarget);
+        if (target == null)
+        {
+            CmdDestroy(theBombTarget);
             readytothrow = false;
         }
         theBombTarget = target;
