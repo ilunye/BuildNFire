3b01fc35b77afd206ec11026d3f7d43725453ee4
ilunye <ilunye93@qq.com>
2024-07-19 08:36:44 +0800

    add network menu and sync workflow

--- Assets/Scripts/Network/GameProps/NetBomb.cs
+++ Assets/Scripts/Network/GameProps/NetBomb.cs
@@ -155,21 +155,21 @@ public class NetBomb : NetworkBehaviour
                     if (nearbyObject.name.EndsWith("1"))
                     {
                         GameObject g = GameObject.Find("PlayerUI_1");
-                        int t = g.GetComponent<WorkFlow>().workFlowPos;
+                        int t = g.GetComponent<NetWorkFlow>().workFlowPos;
                         t--;
                         if (t < 0)
                             t = 0;
-                        g.GetComponent<WorkFlow>().workFlowPos = t;
+                        g.GetComponent<NetWorkFlow>().workFlowPos = t;
                         nearbyObject.GetComponent<NetCannon>().prev_state();
                     }
                     else if (nearbyObject.name.EndsWith("2"))
                     {
                         GameObject g = GameObject.Find("PlayerUI_2");
-                        int t = g.GetComponent<WorkFlow>().workFlowPos;
+                        int t = g.GetComponent<NetWorkFlow>().workFlowPos;
                         t--;
                         if (t < 0)
                             t = 0;
-                        g.GetComponent<WorkFlow>().workFlowPos = t;
+                        g.GetComponent<NetWorkFlow>().workFlowPos = t;
                         nearbyObject.GetComponent<NetCannon>().prev_state();
                     }
                 }
